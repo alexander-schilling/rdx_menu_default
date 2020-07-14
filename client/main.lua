2a1fac4d8073e8bbcd55e5862a5b62a40bf7e1f8
Alexander Schilling <alexander.schilling.miranda@gmail.com>
2020-07-14 01:37:07 -0400

    feature: add sound to inputs
    Really buggy, not recommended

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,5 @@
 RDX = nil
+local lastPressed = "NAV_UP"
 
 Citizen.CreateThread(function()
 	while RDX == nil do
@@ -31,6 +32,8 @@ Citizen.CreateThread(function()
 	AddEventHandler('rdx_menu_default:message:menu_submit', function(data)
 		local menu = RDX.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
 
+		Citizen.InvokeNative(0xCE5D0FFE83939AF1, -1, "SELECT", "HUD_SHOP_SOUNDSET", 1)
+
 		if menu.submit ~= nil then
 			menu.submit(data, menu)
 		end
@@ -38,6 +41,8 @@ Citizen.CreateThread(function()
 
 	AddEventHandler('rdx_menu_default:message:menu_cancel', function(data)
 		local menu = RDX.UI.Menu.GetOpened(MenuType, data._namespace, data._name)
+
+		Citizen.InvokeNative(0xCE5D0FFE83939AF1, -1, "BACK", "HUD_SHOP_SOUNDSET", 1)
 
 		if menu.cancel ~= nil then
 			menu.cancel(data, menu)
@@ -56,6 +61,8 @@ Citizen.CreateThread(function()
 				menu.setElement(i, 'selected', false)
 			end
 		end
+
+		Citizen.InvokeNative(0xCE5D0FFE83939AF1, -1, lastPressed, "HUD_SHOP_SOUNDSET", 1)
 
 		if menu.change ~= nil then
 			menu.change(data, menu)
@@ -77,21 +84,25 @@ Citizen.CreateThread(function()
 			end
 
 			if IsControlPressed(0, 0x6319DB71) and IsInputDisabled(0) and (GetGameTimer() - GUI.Time) > 200 then
+				lastPressed = 'NAV_UP'
 				SendNUIMessage({action  = 'controlPressed', control = 'TOP'})
 				GUI.Time = GetGameTimer()
 			end
 
 			if IsControlPressed(0, 0x05CA7C52) and IsInputDisabled(0) and (GetGameTimer() - GUI.Time) > 200 then
+				lastPressed = 'NAV_DOWN'
 				SendNUIMessage({action  = 'controlPressed', control = 'DOWN'})
 				GUI.Time = GetGameTimer()
 			end
 
 			if IsControlPressed(0, 0xA65EBAB4) and IsInputDisabled(0) and (GetGameTimer() - GUI.Time) > 150 then
+				lastPressed = 'NAV_LEFT'
 				SendNUIMessage({action  = 'controlPressed', control = 'LEFT'})
 				GUI.Time = GetGameTimer()
 			end
 
 			if IsControlPressed(0, 0xDEB34313) and IsInputDisabled(0) and (GetGameTimer() - GUI.Time) > 150 then
+				lastPressed = 'NAV_RIGHT'
 				SendNUIMessage({action  = 'controlPressed', control = 'RIGHT'})
 				GUI.Time = GetGameTimer()
 			end
